7799dc501600a2ef8836ed6b5adc6ad7b9bbeb66
Soichi Hayashi <hayashis@iu.edu>
2020-07-20 03:57:11 -0400

    adding brainlife main
    Former-commit-id: 451dfa709a8c80b9afbfae6db10adb0df09dd335
    Former-commit-id: 3e5012cbea5e0dee306da0573762194469f92bc4
    Former-commit-id: fcfb5aab975b6fdc46989971565e1bc674f78af6

---
+++ CPAC/pipeline/__init__.py
@@ -0,0 +1,7 @@
+from . import cpac_runner
+from . import cpac_group_runner
+from . import cpac_pipeline
+from . import cpac_basc_pipeline
+from . import cpac_cwas_pipeline
+
+__all__ = ['run']
---
+++ CPAC/utils/interfaces/__init__.py
@@ -0,0 +1,13 @@
+from . import function
+from . import masktool
+from . import pc
+from . import brickstat
+from . import datasink
+
+__all__ = [
+    'function',
+    'masktool',
+    'pc',
+    'brickstat',
+    'datasink',
+]
